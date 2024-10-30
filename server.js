a9bc7ba7ee0b4be443d15f3c5f2bbbd82ecdf3f0
Qianni Zhang <zhangqianni24@gmail.com>
2024-10-31 02:12:09 +0800

    cors to connect frontend and backend added in server.js.

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+const cors = require("cors");
+app.use(cors({ origin: /localhost/ }));
+
 const dotenv = require("dotenv");
 dotenv.config();
 
