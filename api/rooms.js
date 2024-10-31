d78c4f66f9f12a8dcd67ccc248904f375acb22f5
Qianni Zhang <zhangqianni24@gmail.com>
2024-11-01 02:36:44 +0800

    Fixed bugs in route - get list of available rooms.

--- api/rooms.js
+++ api/rooms.js
@@ -13,3 +13,50 @@ router.get("/", async (req, res, next) => {
     next(e);
   }
 });
+
+// Get List of Available Rooms with checking the already booked date range.
+router.get("/available", async (req, res, next) => {
+  try {
+    const { fromDate, toDate } = req.query;
+
+    // Transfer dates from strings to dates
+    const start = new Date(fromDate);
+    const end = new Date(toDate);
+
+    const availableRooms = await prisma.room.findMany({
+      where: {
+        // should match the field name in module
+        booking: {
+          none: {
+            OR: [
+              {
+                fromDate: {
+                  lte: end,
+                },
+                toDate: {
+                  gte: start,
+                },
+              },
+            ],
+          },
+        },
+      },
+    });
+    res.json(availableRooms);
+  } catch (e) {
+    next(e);
+  }
+});
+
+// GET/rooms/:id should send a single room according to given ID
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const room = await prisma.room.findUniqueOrThrow({
+      where: { id: +id },
+    });
+    res.json(room);
+  } catch (e) {
+    next(e);
+  }
+});
