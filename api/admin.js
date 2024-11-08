303ceda80989da21bbde8d7195251f2d25e961fb
Qianni Zhang <zhangqianni24@gmail.com>
2024-11-08 23:33:56 +0800

    fixed bugs in getting data for admin panel.

--- api/admin.js
+++ api/admin.js
@@ -10,8 +10,22 @@ const prisma = require("../prisma");
 router.get("/users", isAdmin, async (req, res, next) => {
   try {
     const users = await prisma.user.findMany();
-    // res.status(201).json({ users });
     res.json(users);
+  } catch (e) {
+    next(e);
+  }
+});
+
+// GET/users/:id should send a single user given an ID
+router.get("/users/:id", isAdmin, async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const user = await prisma.user.findUnique({ where: { id: +id } });
+    if (user) {
+      res.json(user);
+    } else {
+      next({ status: 404, message: `User with id ${id} does not exist.` });
+    }
   } catch (e) {
     next(e);
   }
